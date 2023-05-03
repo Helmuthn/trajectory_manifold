1e95a717ec16e29297173d076bc5def7859c1879
Helmuth Naumer <hnaumer2@illinois.edu>
2023-05-03 10:38:56 -0500

    Added docstring for gradient approximation

--- trajectory_manifold/optimize.py
+++ trajectory_manifold/optimize.py
@@ -21,14 +21,40 @@ def simulated_annealing(
 
 
 def zero_order_gradient_estimate(
-        f: Callable,
+        f: Callable[[Float[Array, " dim, batch_size"]], Float[Array, " batch_size"]],
         x: Float[Array, " dim"],
         smoothing: float,
         batch_size: int,
-        key,
-        
+        key: random.KeyArray,
 ) -> Float[Array, " dim"]:
-    """Construct a zero-order estimate of the gradient of a function."""
+    """Construct a zero-order estimate of the gradient of a function.
+    
+    Constructs an approximation of the gradient of a given function through
+    a Monte Carlo method. Generates `batch_size` samples of Gaussian perturbations
+    from the chosen point, and averages the estimates constructed by finite-difference
+    approximations.
+    Includes a smoothing factor which adjusts the variance of the Gaussian 
+    samples.
+
+    Args:
+        f: A function of which the gradient is to be computed.
+        x: The point at which the gradient is computed
+        smoothing: Smoothing factor in the gradient computation, determines the
+          ball size for the random samples
+        batch_size: The number of random points used in the gradient estimate.
+        key: PRNG key for Jax RNG functioanlity
+    
+    Returns:
+        An approximation of the gradient of f at x
+
+    Notes:
+        For more information, see 
+        S. Liu, P. -Y. Chen, B. Kailkhura, G. Zhang, A. O. Hero III and 
+        P. K. Varshney, "A Primer on Zeroth-Order Optimization in Signal 
+        Processing and Machine Learning: Principals, Recent Advances, 
+        and Applications," in IEEE Signal Processing Magazine, vol. 37, 
+        no. 5, pp. 43-54, Sept. 2020, doi: 10.1109/MSP.2020.3003837.
+    """
     center = f(x)
     samples = random.normal(key, x.shape + (batch_size,))
     magnitudes = f(x[:, None] + smoothing * samples) - center
