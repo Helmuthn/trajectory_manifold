0af2c8dbc8516bba024c4cb79bc2447901609ea9
Helmuth Naumer <hnaumer2@illinois.edu>
2023-05-03 10:30:41 -0500

    Added Monte Carlo gradient estimation

--- trajectory_manifold/optimize.py
+++ trajectory_manifold/optimize.py
@@ -6,7 +6,9 @@ posterior distribution of trajectories on the second-order sensitivity
 of the solution to the initial conditions.
 """
 from typing import Callable
-
+from jaxtyping import Float, Array
+import jax.numpy as jnp
+from jax import random
 
 
 def simulated_annealing(
@@ -16,6 +18,22 @@ def simulated_annealing(
 ):
     """Optimize a given function through simulated annealing."""
     pass 
+
+
+def zero_order_gradient_estimate(
+        f: Callable,
+        x: Float[Array, " dim"],
+        smoothing: float,
+        batch_size: int,
+        key,
+        
+) -> Float[Array, " dim"]:
+    """Construct a zero-order estimate of the gradient of a function."""
+    center = f(x)
+    samples = random.normal(key, x.shape + (batch_size,))
+    magnitudes = f(x[:, None] + smoothing * samples) - center
+    estimates = magnitudes * samples 
+    return jnp.sum(estimates, axis=1) / smoothing / batch_size
 
 
 def optimize_gradient(
