6684202546c2737b6342c025ce336f92dca34a74
Helmuth Naumer <hnaumer2@illinois.edu>
2023-05-07 16:35:23 -0500

    Removed plotting code

--- figures/lotka-quant.py
+++ figures/lotka-quant.py
@@ -12,8 +12,6 @@ from math import pi
 
 from tqdm.autonotebook import tqdm
 
-import matplotlib.pyplot as plt
-
 ## Setup Problem Specification
 
 vector_field = examples.lotka_volterra_vector_field(1,2,4,2)
@@ -273,72 +271,6 @@ for i, variance in enumerate(noise_power):
     sup_MAP[i]  /= mcmc_samples 
     outer_bar.update(1)
 
-    
-
-    
-# %%
-SMALL_SIZE = 11
-MEDIUM_SIZE = SMALL_SIZE
-BIGGER_SIZE = 11
-plt.rc('font', size=SMALL_SIZE, family="DejaVu Serif")          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
-fig, axs = plt.subplots(2,2, sharex=True)
-
-fig.set_figwidth(5.5)
-fig.set_figheight(4)
-axs[1,0].plot(noise_power, mae_MMSE, label="MMSE Trajectory, Ambient")
-axs[1,0].plot(noise_power, mae_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[1,0].plot(noise_power, mae_MAP, label="MAP Trajectory")
-axs[1,0].plot(noise_power, mae_ML, label="ML/MAP Initial Condition")
-axs[1,0].plot(noise_power, mae_MMSE_init, label="MMSE Initial Condition")
-axs[1,0].set_xscale('log')
-#axs[1,0].set_yscale('log')
-axs[1,0].set_title("Mean Absolute Error")
-axs[1,0].set_xlim(0.01,10)
-
-axs[1,1].plot(noise_power, mse_MMSE, label="MMSE Trajectory, Ambient")
-axs[1,1].plot(noise_power, mse_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[1,1].plot(noise_power, mse_MAP, label="MAP Trajectory")
-axs[1,1].plot(noise_power, mse_ML, label="ML/MAP Initial Condition")
-axs[1,1].plot(noise_power, mse_MMSE_init, label="MMSE Initial Condition")
-axs[1,1].set_xscale('log')
-#axs[1,1].set_yscale('log')
-axs[1,1].set_title("Mean Squared Error")
-axs[1,1].set_xlim(0.01,10)
-
-axs[0,0].plot(noise_power, sup_MMSE, label="MMSE Trajectory, Ambient")
-axs[0,0].plot(noise_power, sup_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[0,0].plot(noise_power, sup_MAP, label="MAP Trajectory")
-axs[0,0].plot(noise_power, sup_ML, label="ML/MAP Initial Condition")
-axs[0,0].plot(noise_power, sup_MMSE_init, label="MMSE Initial Condition")
-axs[0,0].set_xscale('log')
-#axs[0,0].set_yscale('log')
-axs[0,0].set_title("Sup Norm")
-axs[0,0].set_xlim(0.01,10)
-
-axs[0,1].axis("off")
-
-fig.text(0.5, 0, 'Noise Power', ha='center')
-fig.text(0.0, 0.5, 'Error', va='center', rotation='vertical')
-
-handles, labels = axs[0,0].get_legend_handles_labels()
-axs[0,1].legend(handles, labels, loc='center')
-
-for i in range(2):
-    for j in range(2):
-        axs[i,j].grid(True, which='major')
-
-fig.tight_layout()
-fig.show()
-
-# %%
-fig.savefig("quantitative_snr.pdf", format="pdf", bbox_inches="tight")
 
 #%%
 jnp.savez("quant_noise_lotka.npz", noise_power=noise_power,
@@ -564,73 +496,6 @@ for i, horizon in enumerate(time_horizons):
     outer_bar.update(1)
 
     
-
-    
-# %%
-SMALL_SIZE = 11
-MEDIUM_SIZE = SMALL_SIZE
-BIGGER_SIZE = 11
-plt.rc('font', size=SMALL_SIZE, family="DejaVu Serif")          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
-fig, axs = plt.subplots(2,2, sharex=True)
-
-fig.set_figwidth(5.5)
-fig.set_figheight(4)
-axs[1,0].plot(time_horizons, t_mae_MMSE, label="MMSE Trajectory, Ambient")
-axs[1,0].plot(time_horizons, t_mae_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[1,0].plot(time_horizons, t_mae_MAP, label="MAP Trajectory")
-axs[1,0].plot(time_horizons, t_mae_ML, label="ML/MAP Initial Condition")
-axs[1,0].plot(time_horizons, t_mae_MMSE_init, label="MMSE Initial Condition")
-#axs[1,0].set_xscale('log')
-#axs[1,0].set_yscale('log')
-axs[1,0].set_title("Mean Absolute Error")
-axs[1,0].set_xlim(2,30)
-axs[1,0].set_ylim(0,30)
-
-axs[1,1].plot(time_horizons, t_mse_MMSE, label="MMSE Trajectory, Ambient")
-axs[1,1].plot(time_horizons, t_mse_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[1,1].plot(time_horizons, t_mse_MAP, label="MAP Trajectory")
-axs[1,1].plot(time_horizons, t_mse_ML, label="ML/MAP Initial Condition")
-axs[1,1].plot(time_horizons, t_mse_MMSE_init, label="MMSE Initial Condition")
-#axs[1,1].set_xscale('log')
-#axs[1,1].set_yscale('log')
-axs[1,1].set_title("Mean Squared Error")
-axs[1,1].set_xlim(2,30)
-axs[1,1].set_ylim(0,30)
-
-axs[0,0].plot(time_horizons, t_sup_MMSE, label="MMSE Trajectory, Ambient")
-axs[0,0].plot(time_horizons, t_sup_MMSE_proj, label="MMSE Trajectory, Manifold")
-axs[0,0].plot(time_horizons, t_sup_MAP, label="MAP Trajectory")
-axs[0,0].plot(time_horizons, t_sup_ML, label="ML/MAP Initial Condition")
-axs[0,0].plot(time_horizons, t_sup_MMSE_init, label="MMSE Initial Condition")
-#axs[0,0].set_xscale('log')
-#axs[0,0].set_yscale('log')
-axs[0,0].set_title("Sup Norm")
-axs[0,0].set_xlim(2,30)
-
-axs[0,1].axis("off")
-
-fig.text(0.5, 0, 'Time Horizon', ha='center')
-fig.text(0.0, 0.5, 'Error', va='center', rotation='vertical')
-
-handles, labels = axs[0,0].get_legend_handles_labels()
-axs[0,1].legend(handles, labels, loc='center')
-
-for i in range(2):
-    for j in range(2):
-        axs[i,j].grid(True, which='major')
-
-fig.tight_layout()
-fig.show()
-# %%
-
-fig.savefig("quantitative_horizon.pdf", format="pdf", bbox_inches="tight")
 # %%
 
 jnp.savez("quant_horizon_lotka.npz", time_horizons=time_horizons,
